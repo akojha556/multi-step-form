71514a9916bdd6691ee98fa9a1720d2f5635dd5a
akojha556 <107338576+akojha556@users.noreply.github.com>
2024-04-06 23:57:02 +0530

    form steps complete

---
+++ app/components/step.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Step = (props) => {
+     return (
+          <div className="flex items-center ml-6 mt-6 text-xs">
+               <p className="flex justify-center items-center w-7 h-7 rounded-full mr-2 font-semibold border-light-blue border text-light-blue active:bg-light-blue">{props.itemNo}</p>
+               <div>
+                    <p className="text-white font-thin">STEP {props.itemNo}</p>
+                    <p className="font-medium text-white">{props.content}</p>
+               </div>
+          </div>
+     );
+}
+
+export default Step;
